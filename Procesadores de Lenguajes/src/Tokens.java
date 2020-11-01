f4c36b752928933cdff461ec51a9469f476308a0
Oscar Izquierdo <oscar.izval@gmail.com>
2020-11-01 21:17:00 +0100

    Funcionando tokens en cuanto esten las palabras reservadas

--- Procesadores de Lenguajes/src/Tokens.java
+++ Procesadores de Lenguajes/src/Tokens.java
@@ -1,4 +1,22 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tokens {
+
+	private static List<Pair<String, String>> tokens = new ArrayList<>();
+
+	static void guardarToken(Pair<String, String> token) {
+		tokens.add(token);
+	}
+
+	static void toFile() throws IOException {
+
+		FileWriter myWriter = new FileWriter("tokens.txt");
+		for (Pair<String, String> token : tokens)
+			myWriter.write("<" + token.getLeft() + "," + token.getRight() + ">\n");
+		myWriter.close();
+	}
 
 }
