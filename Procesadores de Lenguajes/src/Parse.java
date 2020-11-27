6f7c8e272ace341eb69117eb14e5e04f9dd32148
Oscar Izquierdo <oscar.izval@gmail.com>
2020-11-27 17:41:19 +0100

    Arreglado el ALEX

---
+++ Procesadores de Lenguajes/src/Parse.java
@@ -0,0 +1,21 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parse {
+
+	private static List<String> secuencia = new ArrayList<>();
+
+	public static void add(String s) {
+		secuencia.add(s);
+	}
+
+	public static void toFile() throws IOException {
+		FileWriter myWriter = new FileWriter("parse.txt");
+		myWriter.write("Descendente");
+		for (String elem : secuencia)
+			myWriter.write(" " + elem);
+		myWriter.close();
+	}
+}
